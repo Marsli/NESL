7d01d829275c4d4b80cc3d8fb6852a13a8cdf879
Maseli <mars.qiangli@gmail.com>
2012-10-29 22:41:07 +0800

    Init Project

---
+++ src/main/java/com/lq/esl/Test.java
@@ -0,0 +1,21 @@
+package com.lq.esl;
+
+import org.freeswitch.esl.client.inbound.Client;
+import org.freeswitch.esl.client.inbound.InboundConnectionFailure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+	
+	private static final Logger log = LoggerFactory.getLogger(Test.class);
+	
+	public static void main(String[] args) {
+		Client client = new Client();
+		try {
+			client.connect("127.0.0.1",8021,"ClueCon",100);
+			log.info("是否可以发送 "+client.canSend());
+		} catch (InboundConnectionFailure e) {
+			e.printStackTrace();
+		}
+	}
+}
